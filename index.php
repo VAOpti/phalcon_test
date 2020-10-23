9fb34f5dd037d79e767b9fe4bf06ac273e574ae3
Ewald van t Veer <evantveer295@student.novacollege.nl>
2020-10-23 13:56:05 +0200

    added base with CRUD functionalities

---
+++ index.php
@@ -0,0 +1,300 @@
+<?php
+
+use Phalcon\Mvc\Micro;
+use Phalcon\Loader;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
+use Phalcon\Http\Response;
+
+$loader = new Loader();
+$loader->registerNamespaces(
+    [
+        'MyApp\Models' => __DIR__ . '/models/',
+    ]
+);
+$loader->register();
+
+$container = new FactoryDefault();
+$container->set(
+    'db',
+    function () {
+        return new PdoMysql(
+            [
+                'host'     => 'localhost',
+                'username' => 'root',
+                'password' => '',
+                'dbname'   => 'robotics',
+            ]
+        );
+    }
+);
+
+$app = new Micro($container);
+
+$app = new Micro();
+
+//show error to client
+//$app->notFound(function () use ($app) {
+//    $app->response->setStatusCode(404, "Not Found")->sendHeaders();
+//    echo 'This is crazy, but this page was not found!';
+//});
+
+// Retrieves all robots
+$app->get(
+    '/api/robots',
+    function () use ($app) {
+        $phql = 'SELECT id, name '
+            . 'FROM MyApp\Models\Robots '
+            . 'ORDER BY name'
+        ;
+
+        $robots = $app
+            ->modelsManager
+            ->executeQuery($phql)
+        ;
+
+        $data = [];
+
+        foreach ($robots as $robot) {
+            $data[] = [
+                'id'   => $robot->id,
+                'name' => $robot->name,
+            ];
+        }
+
+        echo json_encode($data);
+    }
+);
+
+// Searches for robots with $name in their name
+$app->get(
+    '/api/robots/search/{name}',
+    function ($name) use ($app) {
+        $phql = 'SELECT * '
+            . 'FROM MyApp\Models\Robots '
+            . 'WHERE name '
+            . 'LIKE :name: '
+            . 'ORDER BY name'
+        ;
+
+        $robots = $app
+            ->modelsManager
+            ->executeQuery(
+                $phql,
+                [
+                    'name' => '%' . $name . '%'
+                ]
+            )
+        ;
+
+        $data = [];
+
+        foreach ($robots as $robot) {
+            $data[] = [
+                'id'   => $robot->id,
+                'name' => $robot->name,
+            ];
+        }
+
+        echo json_encode($data);
+    }
+);
+
+// Retrieves robots based on primary key
+$app->get(
+    '/api/robots/{id:[0-9]+}',
+    function ($id) use ($app) {
+        $phql = 'SELECT * '
+            . 'FROM MyApp\Models\Robots '
+            . 'WHERE id = :id:'
+        ;
+
+        $robot = $app
+            ->modelsManager
+            ->executeQuery(
+                $phql,
+                [
+                    'id' => $id,
+                ]
+            )
+            ->getFirst()
+        ;
+
+        $response = new Response();
+        if ($robot === false) {
+            $response->setJsonContent(
+                [
+                    'status' => 'NOT-FOUND'
+                ]
+            );
+        } else {
+            $response->setJsonContent(
+                [
+                    'status' => 'FOUND',
+                    'data'   => [
+                        'id'   => $robot->id,
+                        'name' => $robot->name
+                    ]
+                ]
+            );
+        }
+
+        return $response;
+    }
+);
+
+// Adds a new robot
+$app->post(
+    '/api/robots',
+    function () use ($app) {
+        $robot = $app->request->getJsonRawBody();
+        $phql  = 'INSERT INTO MyApp\ModelsRobots '
+            . '(name, type, year) '
+            . 'VALUES '
+            . '(:name:, :type:, :year:)'
+        ;
+
+        $status = $app
+            ->modelsManager
+            ->executeQuery(
+                $phql,
+                [
+                    'name' => $robot->name,
+                    'type' => $robot->type,
+                    'year' => $robot->year,
+                ]
+            )
+        ;
+
+        $response = new Response();
+
+        if ($status->success() === true) {
+            $response->setStatusCode(201, 'Created');
+
+            $robot->id = $status->getModel()->id;
+
+            $response->setJsonContent(
+                [
+                    'status' => 'OK',
+                    'data'   => $robot,
+                ]
+            );
+        } else {
+            $response->setStatusCode(409, 'Conflict');
+
+            $errors = [];
+            foreach ($status->getMessages() as $message) {
+                $errors[] = $message->getMessage();
+            }
+
+            $response->setJsonContent(
+                [
+                    'status'   => 'ERROR',
+                    'messages' => $errors,
+                ]
+            );
+        }
+
+        return $response;
+    }
+);
+
+// Updates robots based on primary key
+$app->put(
+    '/api/robots/{id:[0-9]+}',
+    function ($id) use ($app) {
+        $robot = $app->request->getJsonRawBody();
+        $phql  = 'UPDATE MyApp\Models\Robots '
+            . 'SET name = :name:, type = :type:, year = :year: '
+            . 'WHERE id = :id:';
+
+        $status = $app
+            ->modelsManager
+            ->executeQuery(
+                $phql,
+                [
+                    'id'   => $id,
+                    'name' => $robot->name,
+                    'type' => $robot->type,
+                    'year' => $robot->year,
+                ]
+            )
+        ;
+
+        $response = new Response();
+
+        if ($status->success() === true) {
+            $response->setJsonContent(
+                [
+                    'status' => 'OK'
+                ]
+            );
+        } else {
+            $response->setStatusCode(409, 'Conflict');
+
+            $errors = [];
+            foreach ($status->getMessages() as $message) {
+                $errors[] = $message->getMessage();
+            }
+
+            $response->setJsonContent(
+                [
+                    'status'   => 'ERROR',
+                    'messages' => $errors,
+                ]
+            );
+        }
+
+        return $response;
+    }
+);
+
+// Deletes robots based on primary key
+$app->delete(
+    '/api/robots/{id:[0-9]+}',
+    function ($id) use ($app) {
+        $phql = 'DELETE '
+            . 'FROM MyApp\Models\Robots '
+            . 'WHERE id = :id:';
+
+        $status = $app
+            ->modelsManager
+            ->executeQuery(
+                $phql,
+                [
+                    'id' => $id,
+                ]
+            )
+        ;
+
+        $response = new Response();
+
+        if ($status->success() === true) {
+            $response->setJsonContent(
+                [
+                    'status' => 'OK'
+                ]
+            );
+        } else {
+            $response->setStatusCode(409, 'Conflict');
+
+            $errors = [];
+            foreach ($status->getMessages() as $message) {
+                $errors[] = $message->getMessage();
+            }
+
+            $response->setJsonContent(
+                [
+                    'status'   => 'ERROR',
+                    'messages' => $errors,
+                ]
+            );
+        }
+
+        return $response;
+    }
+);
+
+$app->handle(
+    $_SERVER["REQUEST_URI"]
+);
